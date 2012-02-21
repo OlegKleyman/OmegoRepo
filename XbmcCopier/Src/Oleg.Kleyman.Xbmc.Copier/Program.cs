a9bdb1320d151599d3b5cc53e439fcc9d2d4bbc5
Oleg Kleyman <omego2k@gmail.com>
2012-02-20 22:33:22 -0500

    made rarextractor mockable. The tests fail so far.

--- XbmcCopier/Src/Oleg.Kleyman.Xbmc.Copier/Program.cs
+++ XbmcCopier/Src/Oleg.Kleyman.Xbmc.Copier/Program.cs
@@ -30,8 +30,9 @@ namespace Oleg.Kleyman.Xbmc.Copier
             var release = builder.Build();
             var output = new ReleaseOutput(release, fileName, downloadPath);
             var fileSystem = new FileSystem();
+            var processManager = new ProcessManager();
             var copier = new XbmcFileCopier(XbmcCopierConfigurationSection.DefaultSettings,
-                                            new RarExtractor(XbmcCopierConfigurationSection.DefaultSettings.UnrarPath, fileSystem), fileSystem);
+                                            new RarExtractor(XbmcCopierConfigurationSection.DefaultSettings.UnrarPath, fileSystem, processManager), fileSystem);
             if (release.ReleaseType == ReleaseType.Tv || release.ReleaseType == ReleaseType.Movie)
             {
                 copier.Copy(output);
