92427ea8708c9ec4d430832f9ad5017e94cd109b
Oleg Kleyman <omego2k@gmail.com>
2012-06-12 22:52:40 -0400

    added callback functions and tests

---
+++ Core/Src/Oleg.Kleyman.Winrar.Interop/CallbackProc.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oleg.Kleyman.Winrar.Interop
+{
+    public delegate int CallbackProc(uint msg, IntPtr userData, IntPtr p1, int p2);
+}
