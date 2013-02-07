b41fcafa450e54c3a580e5a836d3c3781e39641c
Oleg Kleyman <omego2k@gmail.com>
2013-02-06 22:54:38 -0500

    refactored and added tests for RarFileProcessor. Refactored RarMemberExtractor and added tests, but they are failing and more refactoring needed on that class.

--- Core/Src/Oleg.Kleyman.Winrar.Core/RarFileProcessor.cs
+++ Core/Src/Oleg.Kleyman.Winrar.Core/RarFileProcessor.cs
@@ -1,4 +1,4 @@
-using System.IO;
+using System;
 using Oleg.Kleyman.Core;
 using Oleg.Kleyman.Winrar.Interop;
 
@@ -6,20 +6,57 @@ namespace Oleg.Kleyman.Winrar.Core
 {
     public class RarFileProcessor
     {
-        public IFileSystem FileSystem { get; set; }
-        private readonly IUnrarHandle _handle;
+        /// <summary>
+        /// Gets the <see cref="IFileSystem"/>  provider for this object.
+        /// </summary>
+        public IFileSystem FileSystem { get; private set; }
 
+        /// <summary>
+        /// Gets the <see cref="IUnrarHandle"/> for this object.
+        /// </summary>
+        public IUnrarHandle Handle { get; private set; }
+
+        /// <summary>
+        /// Initializes an instance of <see cref="RarFileProcessor"/>.
+        /// </summary>
+        /// <param name="handle">The <see cref="IUnrarHandle"/> object to use for operations.</param>
+        /// <param name="fileSystem">The <see cref="IFileSystem"/> object to use for operations.</param>
+        /// <exception cref="ArgumentNullException">Thrown when either the handle or fileSystem argument is null.</exception>
         public RarFileProcessor(IUnrarHandle handle, IFileSystem fileSystem)
         {
+            if (handle == null)
+            {
+                const string handleParamName = "handle";
+                throw new ArgumentNullException(handleParamName);
+            }
+
+            if (fileSystem == null)
+            {
+                const string fileSystemParamName = "fileSystem";
+                throw new ArgumentNullException(fileSystemParamName);
+            }
+
             FileSystem = fileSystem;
-            _handle = handle;
+            Handle = handle;
         }
 
-        public RarStatus ProcessFile(string destinationPath, RARHeaderDataEx headerData)
+        /// <summary>
+        /// Processes a <see cref="RARHeaderDataEx"/> value.
+        /// </summary>
+        /// <param name="destinationPath">The target destination to extract to.</param>
+        /// <returns>The extraction status.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when the <see cref="IUnrarHandle"/> object is not open.</exception>
+        /// <exception cref="UnrarException">Thrown when file could not be extracted.</exception>
+        public RarStatus ProcessFile(string destinationPath)
         {
+            if (!Handle.IsOpen)
+            {
+                const string handleMustBeOpenMessage = "Unrar handle must be open to complete this operation.";
+                throw new InvalidOperationException(handleMustBeOpenMessage);
+            }
             var result =
                 (RarStatus)
-                _handle.UnrarDll.RARProcessFileW(_handle.Handle, (int)ArchiveMemberOperation.Extract, destinationPath,
+                Handle.UnrarDll.RARProcessFileW(Handle.Handle, (int)ArchiveMemberOperation.Extract, destinationPath,
                                                 null);
             if (result != RarStatus.Success)
             {
---
+++ Core/Tests/Unit/Oleg.Kleyman.Winrar.Core.Tests/RarFileProcessorTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using Oleg.Kleyman.Core;
+using Oleg.Kleyman.Tests.Core;
+using Oleg.Kleyman.Winrar.Interop;
+
+namespace Oleg.Kleyman.Winrar.Core.Tests
+{
+    [TestFixture]
+    public class RarFileProcessorTests : TestsBase
+    {
+        private Mock<IUnrarHandle> MockUnrarHandle { get; set; }
+        private Mock<IUnrarDll> MockUnrarDll { get; set; }
+        private Mock<IFileSystem> MockFileSystem { get; set; }
+
+        private RARHeaderDataEx MockHeaderData { get; set; }
+
+        public override void Setup()
+        {
+            MockUnrarHandle = new Mock<IUnrarHandle>();
+            MockUnrarDll = new Mock<IUnrarDll>();
+            MockFileSystem = new Mock<IFileSystem>();
+            MockUnrarHandle.SetupGet(x => x.UnrarDll).Returns(MockUnrarDll.Object);
+            MockUnrarHandle.SetupGet(x => x.Handle).Returns(new IntPtr(1337));
+            MockHeaderData = new RARHeaderDataEx
+            {
+                ArcName = "㩃䝜瑩敒潰屳慍湩敄慦汵屴潃浭湯呜獥屴敔瑳瀮牡ㅴ爮牡",
+                ArcNameW = "C:\\GitRepos\\MainDefault\\Common\\Test\\Test.part1.rar",
+                CmtBuf = null,
+                CmtBufSize = 1,
+                CmtSize = 0,
+                CmtState = 0,
+                FileAttr = 32,
+                FileCRC = 2631502099,
+                FileName = "整瑳⸲硴t",
+                FileNameW = "test2.txt",
+                FileTime = 1087152912,
+                Flags = 37058,
+                HostOS = 2,
+                Method = 48,
+                PackSize = 3145642,
+                PackSizeHigh = 0,
+                Reserved = new uint[1024],
+                UnpSize = 5293080,
+                UnpSizeHigh = 0,
+                UnpVer = 20
+            };
+
+            MockUnrarDll.Setup(x => x.RARProcessFileW(new IntPtr(1337), 2, @"C:\invalidExtractPath", null))
+                        .Returns((int)RarStatus.BadData);
+        }
+
+        [SetUp]
+        public void TestSetup()
+        {
+            MockUnrarHandle.SetupGet(x => x.IsOpen).Returns(true);
+        }
+        private RarFileProcessor GetFileProcessor()
+        {
+            return new RarFileProcessor(MockUnrarHandle.Object, MockFileSystem.Object);
+        }
+
+        [Test]
+        public void ProcessFileShouldCompleteSuccessfully()
+        {
+            var fileProcessor = GetFileProcessor();
+            var result = fileProcessor.ProcessFile(@"C:\validExtractPath");
+            Assert.That(result, Is.EqualTo(RarStatus.Success));
+        }
+
+        [Test]
+        public void ProcessFileShouldFailBecauseOfBadData()
+        {
+            var fileProcessor = GetFileProcessor();
+            var ex = Assert.Throws<UnrarException>(() => fileProcessor.ProcessFile(@"C:\invalidExtractPath"));
+            Assert.That(ex.Status, Is.EqualTo(RarStatus.BadData));
+            Assert.That(ex.Message, Is.EqualTo("Unable to extract file."));
+        }
+
+        [Test]
+        public void ProcessFileShouldFailBecauseHandleIsClosed()
+        {
+            var fileProcessor = GetFileProcessor();
+            MockUnrarHandle.SetupGet(x => x.IsOpen).Returns(false);
+            var ex = Assert.Throws<InvalidOperationException>(() => fileProcessor.ProcessFile(null));
+            Assert.That(ex.Message, Is.EqualTo("Unrar handle must be open to complete this operation."));
+        }
+
+        [Test]
+        public void ConstructorShouldThrowArgumentNullExceptionWhenTheHandleArgumentIsNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new RarFileProcessor(null, null));
+            Assert.That(ex.ParamName, Is.EqualTo("handle"));
+        }
+
+        [Test]
+        public void ConstructorShouldThrowArgumentNullExceptionWhenTheFileSystemArgumentIsNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => new RarFileProcessor(MockUnrarHandle.Object, null));
+            Assert.That(ex.ParamName, Is.EqualTo("fileSystem"));
+        }
+    }
+}
