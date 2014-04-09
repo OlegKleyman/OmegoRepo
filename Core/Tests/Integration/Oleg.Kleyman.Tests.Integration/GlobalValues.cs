b4d88d86ec9f9917c54cbd8ea97bc28e06bc9d2b
Oleg Kleyman <omego2k@gmail.com>
2014-04-08 20:43:48 -0400

    added utorrent steps and global constants files to common integration project

---
+++ Core/Tests/Integration/Oleg.Kleyman.Tests.Integration/GlobalValues.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+namespace Oleg.Kleyman.Tests.Integration
+{
+    public static class GlobalValues
+    {
+        /// <summary>
+        /// Gets the root repository path based on current working directory
+        /// </summary>
+        /// <remarks>
+        /// This will only return the correct path when used from a test 
+        /// project following the conventional directory structure and
+        /// with an unaltered working directory. 
+        /// </remarks>
+        public static string RepositoryPath
+        {
+            get { return Path.GetFullPath(@"..\..\..\..\..\..\"); }
+        }
+
+        /// <summary>
+        /// Gets the path to UTorrent based on the <see cref="RepositoryPath"/> property
+        /// </summary>
+        public static string UTorrentPath
+        {
+            get { return Path.Combine(RepositoryPath, @"Common\Test\TorrentTest\Utorrent\uTorrent.exe"); }
+        }
+
+        /// <summary>
+        /// Gets the path to torrent files based on the <see cref="RepositoryPath"/> property
+        /// </summary>
+        public static string TorrentFilesPath
+        {
+            get { return Path.Combine(RepositoryPath, @"Common\Test\TorrentTest\torrents"); }
+        }
+
+        /// <summary>
+        /// Gets the path to torrent download directory based on the <see cref="RepositoryPath"/> property
+        /// </summary>
+        public static string DownloadPath
+        {
+            get { return Path.Combine(RepositoryPath, @"Common\Test\TorrentTest\DownloadDirectory"); }
+        }
+    }
+}
---
+++ Core/Tests/Integration/Oleg.Kleyman.Tests.Integration/UtorrentSteps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using TechTalk.SpecFlow;
+
+namespace Oleg.Kleyman.Tests.Integration
+{
+    public class UtorrentSteps
+    {
+        [Given(@"UTorrent is running")]
+        public void GivenIAddedAllTorrents()
+        {
+            var utorrentPath = Path.GetFullPath(@"..\..\..\..\..\..\Common\Test\TorrentTest\Utorrent\uTorrent.exe");
+            var process = Process.Start(utorrentPath);
+            if (process == null)
+            {
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Could not start utorrent process at path {0}", utorrentPath));
+            }
+        }
+
+        [When(@"I add ([.\\\w]+) torrent")]
+        public void WhenAddTorrent(string torrentPath)
+        {
+            const string arguments = "/directory {0} {1}";
+
+            var process = Process.Start(GlobalValues.UTorrentPath, string.Format(CultureInfo.InvariantCulture,
+                                                                                 arguments,
+                                                                                 GlobalValues.DownloadPath,
+                                                                                 GlobalValues.TorrentFilesPath));
+            if (process == null)
+            {
+                throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Could not start utorrent process at path {0}", GlobalValues.UTorrentPath));
+            }
+        }
+    }
+}
