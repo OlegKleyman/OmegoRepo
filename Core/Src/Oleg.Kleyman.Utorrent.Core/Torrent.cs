79343006d69512f4e1044f80964ab94e38ca8114
Oleg Kleyman <omego2k@gmail.com>
2014-04-10 19:40:40 -0400

    added Torrents to TorrentList and made it more testable

--- Core/Src/Oleg.Kleyman.Utorrent.Core/Torrent.cs
+++ Core/Src/Oleg.Kleyman.Utorrent.Core/Torrent.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.Serialization;
 
@@ -9,11 +10,38 @@ namespace Oleg.Kleyman.Utorrent.Core
     ///     Represents a torrent.
     /// </summary>
     [DataContract]
-    public class Torrent : UTorrentBase
+    public class Torrent : UTorrentBase, ICloneable
     {
-        private object[] _files;
-        private TorrentFile[] _torrentFiles;
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Torrent()
+        {
+            
+        }
+        /// <summary>
+        /// Initializes the object with a <see cref="TorrentHash"/> and <see cref="files"/>
+        /// </summary>
+        /// <param name="hash">The hash of the torrent.</param>
+        /// <param name="torrentFiles">File contents of the torrent.</param>
+        public Torrent(TorrentHash hash, TorrentFile[] torrentFiles)
+        {
+            if (hash == null)
+            {
+                throw new ArgumentNullException("hash");
+            }
 
+            if (torrentFiles == null)
+            {
+                throw new ArgumentNullException("torrentFiles");
+            }
+
+            Hash = hash;
+            TorrentFiles = torrentFiles;
+        }
+
+        private object[] _files;
+        
         /// <summary>
         ///     Gets or sets the torrent files.
         /// </summary>
@@ -34,6 +62,8 @@ namespace Oleg.Kleyman.Utorrent.Core
         /// Gets the torrent <see cref="Hash"/>
         /// </summary>
         public TorrentHash Hash { get; private set; }
+
+        private TorrentFile[] _torrentFiles;
 
         /// <summary>
         /// Gets an array of <see cref="TorrentFile"/>s.
@@ -56,6 +86,50 @@ namespace Oleg.Kleyman.Utorrent.Core
             TorrentFiles = (from object[] file in value.Skip(1)
                             from object[] properties in file
                             select (TorrentFile) properties).ToArray();
+        }
+
+        /// <summary>
+        /// Casts an object array to a <see cref="Torrent"/> object.
+        /// </summary>
+        /// <param name="targetTorrent">The target <see cref="object"/> array to cast.</param>
+        /// <returns>A <see cref="UtorrentRssFeed"/> object</returns>
+        /// <exception cref="InvalidCastException">When the target array format is invalid.</exception>
+        public static explicit operator Torrent(object[] targetTorrent)
+        {
+            const int expectedLength = 29;
+            if (targetTorrent.Length != expectedLength)
+            {
+                throw new InvalidCastException(string.Format(CultureInfo.InvariantCulture, "Unexpected array length of {0}, {1} expected", targetTorrent.Length, expectedLength));
+            }
+            var torrent = new Torrent(TorrentHash.Parse(targetTorrent[0].ToString()), new TorrentFile[0]);
+            
+            return torrent;
+        }
+
+        /// <summary>
+        /// Serializes the current object instance into an object array
+        /// compatible with the UTorrent API.
+        /// </summary>
+        /// <returns></returns>
+        public object[] ToArray()
+        {
+            var serializedArray = new object[29];
+            serializedArray[0] = Hash.Value;
+            return serializedArray;
+        }
+
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a copy of this instance.
+        /// </returns>
+        /// <filterpriority>2</filterpriority>
+        public object Clone()
+        {
+            var torrent = new Torrent(TorrentHash.Parse(Hash.Value), new TorrentFile[0]);
+            
+            return torrent;
         }
     }
 }
--- Core/Src/Oleg.Kleyman.Utorrent.Core/UtorrentList.cs
+++ Core/Src/Oleg.Kleyman.Utorrent.Core/UtorrentList.cs
@@ -15,15 +15,22 @@ namespace Oleg.Kleyman.Utorrent.Core
         /// Initializes the object.
         /// </summary>
         /// <param name="feeds">An array of <see cref="UtorrentRssFeed"/> object to initialize the object with.</param>
-        public UTorrentList(UtorrentRssFeed[] feeds)
+        /// <param name="torrents">An array of <see cref="Torrent"/> object to initialize the object with.</param>
+        public UTorrentList(UtorrentRssFeed[] feeds, Torrent[] torrents)
         {
             if (feeds == null)
             {
                 const string feedParamName = "feeds";
                 throw new ArgumentNullException(feedParamName);
             }
+            if (torrents == null)
+            {
+                const string torrentsParamName = "torrents";
+                throw new ArgumentNullException(torrentsParamName);
+            }
 
             RssFeeds = feeds;
+            Torrents = torrents;
         }
 
         private object[][] _list;
@@ -61,6 +68,41 @@ namespace Oleg.Kleyman.Utorrent.Core
             }
         }
 
+        private object[][] _torrentList;
+
+        /// <summary>
+        /// Gets or sets the Torrent property of this object. Converts an object array to <see cref="Torrent"/>
+        /// or gets the object array representation of the Torrent property.
+        /// </summary>
+        /// <remarks>
+        /// UTorrent api returns everything in an array. This mess should not be exposed to the public API. 
+        /// Use Torrent property when accessing the public API instead. This property cannot be unit tested.
+        /// </remarks>
+        [DataMember(Name = "torrents")]
+        private object[][] TorrentsApi
+        {
+            get
+            {
+                if (_torrentList != null)
+                {
+                    return _torrentList;
+                }
+                _torrentList = new object[Torrents.Length][];
+                for (int i = 0; i < Torrents.Length; i++)
+                {
+                    _torrentList[i] = Torrents[i].ToArray();
+                }
+                return _torrentList;
+            }
+            set
+            {
+                //only setting the RssFeeds property here.
+                //This is needed for cleanliness because the Utorrent API
+                //returns an object array.
+                Torrents = value.Select(torrent => (Torrent)torrent).ToArray();
+            }
+        }
+
         private UtorrentRssFeed[] _rssFeeds;
         
         /// <summary>
@@ -73,7 +115,22 @@ namespace Oleg.Kleyman.Utorrent.Core
                 //Returning the cloned object so it doesn't get modified by the consumer.
                 return _rssFeeds.Select(feed => (UtorrentRssFeed) feed.Clone()).ToArray();
             }
-            set { _rssFeeds = value; }
+            private set { _rssFeeds = value; }
+        }
+
+        private Torrent[] _torrents;
+
+        /// <summary>
+        /// Gets the Torrents
+        /// </summary>
+        public Torrent[] Torrents
+        {
+            get
+            {
+                //Returning the cloned object so it doesn't get modified by the consumer.
+                return _torrents.Select(torrent => (Torrent)torrent.Clone()).ToArray();
+            }
+            private set { _torrents = value; }
         }
     }
 }
