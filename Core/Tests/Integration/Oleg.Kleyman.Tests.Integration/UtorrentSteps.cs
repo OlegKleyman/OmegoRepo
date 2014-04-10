64b2166a09eb83a709a5e563ce24931b2952145e
Oleg Kleyman <omego2k@gmail.com>
2014-04-10 19:52:54 -0400

    added common utorrent steps

--- Core/Tests/Integration/Oleg.Kleyman.Tests.Integration/UtorrentSteps.cs
+++ Core/Tests/Integration/Oleg.Kleyman.Tests.Integration/UtorrentSteps.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Globalization;
 using System.IO;
+using Oleg.Kleyman.Core.Linq;
+using Oleg.Kleyman.Utorrent.Core;
 using TechTalk.SpecFlow;
 
 namespace Oleg.Kleyman.Tests.Integration
@@ -9,6 +11,24 @@ namespace Oleg.Kleyman.Tests.Integration
     [Binding]
     public class UtorrentSteps : Steps
     {
+        private const string UTORRENT_SERVICE_KEY = "UTORRENT_SERVICE";
+        private const string SERVICE_TOKEN_KEY = "SERVICE_TOKEN";
+
+        [BeforeFeature]
+        public void BeforeFeature()
+        {
+            var builder = new UtorrentServiceBuilder(new DefaultSettings());
+            FeatureContext.Current.Set(builder.GetService(), UTORRENT_SERVICE_KEY);
+        }
+
+        [When(@"I call the method GetKey")]
+        [Given(@"I have attained an API key")]
+        public void GivenIHaveAttainedAnApiKey()
+        {
+            var service = FeatureContext.Current.Get<IUtorrentService>(UTORRENT_SERVICE_KEY);
+            ScenarioContext.Current.Set(service.GetKey(), SERVICE_TOKEN_KEY);
+        }
+
         [Given(@"UTorrent is running")]
         public void GivenIAddedAllTorrents()
         {
@@ -33,6 +53,27 @@ namespace Oleg.Kleyman.Tests.Integration
             {
                 throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "Could not start utorrent process at path {0}", GlobalValues.UTorrentPath));
             }
+        }
+
+        [AfterScenario]
+        public void AfterUTorrentScenario()
+        {
+            var service = FeatureContext.Current.Get<IUtorrentService>(UTORRENT_SERVICE_KEY);
+            var token = ScenarioContext.Current.Get<string>(SERVICE_TOKEN_KEY);
+
+            var torrentList = service.GetList(token);
+            foreach (var torrent in torrentList.Torrents)
+            {
+                service.Remove(token, torrent.Hash.ToString());
+            }
+
+            var utorrentProcesses = Process.GetProcessesByName("utorrent");
+            if (utorrentProcesses.Length == 0)
+            {
+                throw new InvalidOperationException("Could not locate processes with the name utorrent");
+            }
+
+            utorrentProcesses.ForEach(process => process.Kill());
         }
     }
 }
