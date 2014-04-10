906587aa70a5f98938634dc4c7a00ffaf20edfaa
Oleg Kleyman <omego2k@gmail.com>
2014-04-10 19:48:31 -0400

    updated UTorrentList tests

--- Core/Tests/Unit/Oleg.Kleyman.Utorrent.Core.Tests/UTorrentListTests.cs
+++ Core/Tests/Unit/Oleg.Kleyman.Utorrent.Core.Tests/UTorrentListTests.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using Oleg.Kleyman.Tests.Core;
 
@@ -12,21 +7,28 @@ namespace Oleg.Kleyman.Utorrent.Core.Tests
     [TestFixture]
     public class UTorrentListTests : TestsBase
     {
+        private UtorrentRssFeed _utorrentRssFeed;
+        private Torrent _torrent;
+
+        [TestFixtureSetUp]
         public override void Setup()
         {
-            
+            _utorrentRssFeed = new UtorrentRssFeed
+            {
+                Id = 1,
+                Name = "television",
+                Url = new Uri("http://www.tvtorrents.com/mytaggedRSS?digest=fe63619e27875fe75ccdaf5968dd094a1397a8b8&hash=16384e56b48898aaf5a3fde5595e2a3a8e2060a7&include=(720p%7C1080p)&interval=12+days&exclude=(season)")
+            };
+
+            var file = new TorrentFile();
+            file.Name = "testFile.mkv";
+            _torrent = new Torrent(TorrentHash.Parse("FB4F76083F21CC6AA6A2E2EB210D126C3CC090DC"), new []{ file });
         }
 
         [Test]
         public void ConstructorShouldSetRssFeedProperty()
         {
-            var feed = new UtorrentRssFeed
-                {
-                    Id = 1,
-                    Name = "television",
-                    Url = new Uri("http://www.tvtorrents.com/mytaggedRSS?digest=fe63619e27875fe75ccdaf5968dd094a1397a8b8&hash=16384e56b48898aaf5a3fde5595e2a3a8e2060a7&include=(720p%7C1080p)&interval=12+days&exclude=(season)")
-                };
-            var utorrentList = new UTorrentList(new []{ feed});
+            var utorrentList = GetUtorrentList(_utorrentRssFeed, _torrent);
             
             Assert.That(utorrentList.RssFeeds, Is.Not.Null);
             Assert.That(utorrentList.RssFeeds[0].Name, Is.EqualTo("television"));
@@ -35,16 +37,22 @@ namespace Oleg.Kleyman.Utorrent.Core.Tests
         }
 
         [Test]
-        public void RssFeedPropertyShouldReturnAClonedInstance()
+        public void RssFeedsPropertyShouldReturnAClonedInstance()
         {
-            var feed = new UtorrentRssFeed
-            {
-                Id = 1,
-                Name = "television",
-                Url = new Uri("http://www.tvtorrents.com/mytaggedRSS?digest=fe63619e27875fe75ccdaf5968dd094a1397a8b8&hash=16384e56b48898aaf5a3fde5595e2a3a8e2060a7&include=(720p%7C1080p)&interval=12+days&exclude=(season)")
-            };
-            var utorrentList = new UTorrentList(new[]{ feed });
-            Assert.That(utorrentList.RssFeeds[0], Is.Not.SameAs(feed));
+            var utorrentList = GetUtorrentList(_utorrentRssFeed, _torrent);
+            Assert.That(utorrentList.RssFeeds[0], Is.Not.SameAs(_utorrentRssFeed));
+        }
+
+        [Test]
+        public void TorrentsPropertyShouldReturnAClonedInstance()
+        {
+            var utorrentList = GetUtorrentList(_utorrentRssFeed, _torrent);
+            Assert.That(utorrentList.Torrents[0], Is.Not.SameAs(_torrent));
+        }
+
+        private static UTorrentList GetUtorrentList(UtorrentRssFeed feed, Torrent torrent)
+        {
+            return new UTorrentList(new[] { feed }, new[] { torrent });
         }
     }
 }
